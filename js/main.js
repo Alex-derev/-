a13db2bde09be4accb070fd87c1d6fe6a32faab5
Alex-derev <60820604+Alex-derev@users.noreply.github.com>
2020-05-06 20:52:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -38,7 +38,8 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //timer
 
-    let deadline = '2020-10-05';
+    let deadline = '2018-11-21';
+
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -64,12 +65,22 @@ window.addEventListener('DOMContentLoaded', function () {
             function updateClock() {
                 let t = getTimeRemaining(endtime);
 
-                hours.textContent = t.hours;
-                minutes.textContent = t.minutes;
-                seconds.textContent = t.seconds;
+                function addZero(num) {
+                    if(num <= 9) {
+                        return '0' + num;
+                    } else return num;
+                };
+
+                hours.textContent = addZero(t.hours);
+                minutes.textContent = addZero(t.minutes);
+                seconds.textContent = addZero(t.seconds);
 
                 if (t.total <= 0) {
                     clearInterval(timeInterval);
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+
                 }
             };
 
